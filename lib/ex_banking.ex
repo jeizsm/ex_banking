fc0fa07dc51719670dd765ae5afb47c85b2d3218
Marat Safin <jeizsm@gmail.com>
2021-09-24 09:40:57 +0300

    some tests and dependecies

--- lib/ex_banking.ex
+++ lib/ex_banking.ex
@@ -24,12 +24,11 @@ defmodule ExBanking do
       Requests for user A should not affect to performance of requests to user B (maybe except send function when both A and B users are involved in the request)
   """
 
-
   @doc """
     Function creates new user in the system
     New user has zero balance of any currency
   """
-  @spec create_user(user :: String.t) :: :ok | {:error, :wrong_arguments | :user_already_exists}
+  @spec create_user(user :: String.t()) :: :ok | {:error, :wrong_arguments | :user_already_exists}
   def create_user(_user) do
     {:error, :wrong_arguments}
   end
@@ -38,7 +37,9 @@ defmodule ExBanking do
     Increases user’s balance in given currency by amount value
     Returns new_balance of the user in given format
   """
-  @spec deposit(user :: String.t, amount :: number, currency :: String.t) :: {:ok, new_balance :: number} | {:error, :wrong_arguments | :user_does_not_exist | :too_many_requests_to_user}
+  @spec deposit(user :: String.t(), amount :: number, currency :: String.t()) ::
+          {:ok, new_balance :: number}
+          | {:error, :wrong_arguments | :user_does_not_exist | :too_many_requests_to_user}
   def deposit(_user, _amount, _currency) do
     {:error, :wrong_arguments}
   end
@@ -47,7 +48,13 @@ defmodule ExBanking do
     Decreases user’s balance in given currency by amount value
     Returns new_balance of the user in given format
   """
-  @spec withdraw(user :: String.t, amount :: number, currency :: String.t) :: {:ok, new_balance :: number} | {:error, :wrong_arguments | :user_does_not_exist | :not_enough_money | :too_many_requests_to_user}
+  @spec withdraw(user :: String.t(), amount :: number, currency :: String.t()) ::
+          {:ok, new_balance :: number}
+          | {:error,
+             :wrong_arguments
+             | :user_does_not_exist
+             | :not_enough_money
+             | :too_many_requests_to_user}
   def withdraw(_user, _amount, _currency) do
     {:error, :wrong_arguments}
   end
@@ -55,7 +62,9 @@ defmodule ExBanking do
   @doc """
     Returns balance of the user in given format
   """
-  @spec get_balance(user :: String.t, currency :: String.t) :: {:ok, balance :: number} | {:error, :wrong_arguments | :user_does_not_exist | :too_many_requests_to_user}
+  @spec get_balance(user :: String.t(), currency :: String.t()) ::
+          {:ok, balance :: number}
+          | {:error, :wrong_arguments | :user_does_not_exist | :too_many_requests_to_user}
   def get_balance(_user, _currency) do
     {:error, :wrong_arguments}
   end
@@ -65,7 +74,20 @@ defmodule ExBanking do
     Increases to_user’s balance in given currency by amount value
     Returns balance of from_user and to_user in given format
   """
-  @spec send(from_user :: String.t, to_user :: String.t, amount :: number, currency :: String.t) :: {:ok, from_user_balance :: number, to_user_balance :: number} | {:error, :wrong_arguments | :not_enough_money | :sender_does_not_exist | :receiver_does_not_exist | :too_many_requests_to_sender | :too_many_requests_to_receiver}
+  @spec send(
+          from_user :: String.t(),
+          to_user :: String.t(),
+          amount :: number,
+          currency :: String.t()
+        ) ::
+          {:ok, from_user_balance :: number, to_user_balance :: number}
+          | {:error,
+             :wrong_arguments
+             | :not_enough_money
+             | :sender_does_not_exist
+             | :receiver_does_not_exist
+             | :too_many_requests_to_sender
+             | :too_many_requests_to_receiver}
   def send(_from_user, _to_user, _amount, _currency) do
     {:error, :wrong_arguments}
   end
--- test/ex_banking_test.exs
+++ test/ex_banking_test.exs
@@ -2,7 +2,115 @@ defmodule ExBankingTest do
   use ExUnit.Case
   doctest ExBanking
 
-  test "greets the world" do
-    assert ExBanking.hello() == :world
+  test "create user" do
+    assert ExBanking.create_user("user") == :ok
+    assert ExBanking.create_user("USER") == {:ok}
+  end
+
+  test "create user that already exists" do
+    ExBanking.create_user("user")
+    assert ExBanking.create_user("user") == {:error, :user_already_exists}
+  end
+
+  test "wrong arguments" do
+    assert ExBanking.create_user(1) == {:error, :wrong_arguments}
+    assert ExBanking.create_user(<<239, 191, 19>>) == {:error, :wrong_arguments}
+
+    assert ExBanking.deposit("user", "100", "USD") == {:error, :wrong_arguments}
+    assert ExBanking.deposit("user", 100, <<239, 191, 19>>) == {:error, :wrong_arguments}
+    assert ExBanking.deposit(<<239, 191, 19>>, 100, "USD") == {:error, :wrong_arguments}
+
+    assert ExBanking.withdraw("user", "100", "USD") == {:error, :wrong_arguments}
+    assert ExBanking.withdraw("user", 100, <<239, 191, 19>>) == {:error, :wrong_arguments}
+    assert ExBanking.withdraw(<<239, 191, 19>>, 100, "USD") == {:error, :wrong_arguments}
+
+    assert ExBanking.get_balance("user", <<239, 191, 19>>) == {:error, :wrong_arguments}
+    assert ExBanking.get_balance(<<239, 191, 19>>, "USD") == {:error, :wrong_arguments}
+
+    assert ExBanking.send("user", <<239, 191, 19>>, 100, "USD") == {:error, :wrong_arguments}
+    assert ExBanking.send(<<239, 191, 19>>, "user2", 100, "USD") == {:error, :wrong_arguments}
+    assert ExBanking.send("user", "user2", 100, <<239, 191, 19>>) == {:error, :wrong_arguments}
+    assert ExBanking.send("user", "user2", "100", "USD") == {:error, :wrong_arguments}
+    assert ExBanking.send("user", "user", 100, "USD") == {:error, :wrong_arguments}
+  end
+
+  test "user does not exist" do
+    assert ExBanking.deposit("user", 100, "USD") == {:error, :user_does_not_exist}
+    assert ExBanking.withdraw("user", 100, "USD") == {:error, :user_does_not_exist}
+    assert ExBanking.get_balance("user", "USD") == {:error, :user_does_not_exist}
+    assert ExBanking.send("user", "user2", 100, "USD") == {:error, :sender_does_not_exist}
+    ExBanking.create_user("user")
+    assert ExBanking.send("user", "user2", 100, "USD") == {:error, :receiver_does_not_exist}
+  end
+
+  test "too many requests to user" do
+    functions = [
+      &ExBanking.deposit/3,
+      &ExBanking.get_balance/2,
+      &ExBanking.withdraw/3,
+      &ExBanking.send/4
+    ]
+
+    args = [
+      get_balance: ["user", 100],
+      deposit: ["user", 100, "USD"],
+      withdraw: ["user", 100, "USD"],
+      send: ["user", "user2", 100, "USD"]
+    ]
+
+    errors = [
+      {:error, :too_many_requests_to_user},
+      {:error, :too_many_requests_to_sender},
+      {:error, :too_many_requests_to_receiver}
+    ]
+
+    ExBanking.create_user("user")
+    ExBanking.create_user("user2")
+
+    1..100 |> Enum.map(fn _ -> Enum.random(functions) end)
+
+    tasks =
+      Task.async_stream(functions, fn function ->
+        name = Function.info(function)[:name]
+        apply(function, args[name])
+      end)
+      |> Enum.to_list()
+
+    assert Enum.any?(tasks, fn elem -> Enum.any?(errors, elem) end) == true
+  end
+
+  test "not enough money" do
+    ExBanking.create_user("user")
+    ExBanking.create_user("user2")
+    assert ExBanking.withdraw("user", 0.1, "USD") == {:error, :not_enough_money}
+    ExBanking.deposit("user", 0.1, "USD")
+    assert ExBanking.withdraw("user", 0.1, "usd") == {:error, :not_enough_money}
+    assert ExBanking.send("user", "user2", 0.2, "USD") == {:error, :not_enough_money}
+  end
+
+  test "deposit money" do
+    ExBanking.create_user("user")
+    assert ExBanking.deposit("user", 0.1, "USD") == {:ok, 0.1}
+    assert ExBanking.deposit("user", 0.2, "USD") == {:ok, 0.3}
+    assert ExBanking.deposit("user", 0.2, "usd") == {:ok, 0.2}
+  end
+
+  test "get balance" do
+    ExBanking.create_user("user")
+    ExBanking.deposit("user", 0.1, "USD")
+    assert ExBanking.get_balance("user", "USD") == {:ok, 0.1}
+  end
+
+  test "withdraw money" do
+    ExBanking.create_user("user")
+    ExBanking.deposit("user", 0.3, "USD")
+    assert ExBanking.withdraw("user", 0.1, "USD") == {:ok, 0.2}
+  end
+
+  test "send money" do
+    ExBanking.create_user("user")
+    ExBanking.create_user("user2")
+    ExBanking.deposit("user", 0.3, "USD")
+    assert ExBanking.send("user", "user2", 0.1, "USD") == {:ok, 0.2, 0.1}
   end
 end
